bf3c815ed5f52289b5402cb4257016cf0bf8f1c9
jovig7050 <jovig7050@oslomet.no>
2024-11-05 14:24:34 +0100

    Fulført javascript for å skjule fag fra andre linjer

--- scripts/toggleFag.js
+++ scripts/toggleFag.js
@@ -1,26 +1,33 @@
-const 
-toggleDisplay = (className,
-display) 
-=> {
-
-[...document.getElementsByClassName(className)].forEach(element
-=> {
-
-element.style.display
-= 
-display;
-
-});
-
+const toggleDisplay = (className, display) => {
+  [...document.getElementsByClassName(className)].forEach(element=> { 
+    element.style.display=display; 
+  });
 };
 
+const showCommon = () => {
+  toggleDisplay('ifMatematikkOgFysikkShown', 'none');
+  toggleDisplay('ifStatistiskeMetoderShown', 'none');
+  toggleDisplay('ifVitenskapligeBeregningerShown', 'none');
+  toggleDisplay('ifAllShown', 'block');
+}
 
-const 
-hideCommon = () 
-=> toggleDisplay('all',
-'none');
+const showMatematikkOgFysikk = () => {
+  toggleDisplay('ifAllShown', 'none');
+  toggleDisplay('ifStatistiskeMetoderShown', 'none');
+  toggleDisplay('ifVitenskapligeBeregningerShown', 'none');
+  toggleDisplay('ifMatematikkOgFysikkShown', 'block');
+}
 
-const 
-hideAsdf = () 
-=> toggleDisplay('all',
-'none');
+const showStatistiskeMetoder = () => {
+  toggleDisplay('ifMatematikkOgFysikkShown', 'none');
+  toggleDisplay('ifAllShown', 'none');
+  toggleDisplay('ifVitenskapligeBeregningerShown', 'none');
+  toggleDisplay('ifStatistiskeMetoderShown', 'block');
+}
+
+const showVitenskapligeBeregninger = () => {
+  toggleDisplay('ifMatematikkOgFysikkShown', 'none');
+  toggleDisplay('ifAllShown', 'none');
+  toggleDisplay('ifStatistiskeMetoderShown', 'none');
+  toggleDisplay('ifVitenskapligeBeregningerShown', 'block');
+}
